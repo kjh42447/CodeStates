f3d34d5235cca8248249e311ecf82cb403f11244
kjh42447 <kjh42447@naver.com>
2022-08-23 22:18:07 +0900

    powerSet

--- section1/Java/JavaTest/src/Test.java
+++ section1/Java/JavaTest/src/Test.java
@@ -3,43 +3,58 @@ import java.util.stream.Collectors;
 
 public class Test {
 
-    public static boolean balancedBrackets(String str) {
+    public static ArrayList<String> powerSet(String str) {
         // TODO:
-        ArrayList<Character> brackets = new ArrayList<>();
-        for(int i = 0; i < str.length(); i++) {
-            char c = str.charAt(i);
-            if (c == '(' || c == '{' || c == '[') {
-                brackets.add(c);
-            }
-            else if (c == ')') {
-                if (brackets.size() == 0)   return false;
-                else if (!(brackets.get(brackets.size() - 1) == '('))   return false;
-                brackets.remove(brackets.size()-1);
-            }
-            else if (c == '}') {
-                if (brackets.size() == 0)   return false;
-                else if (!(brackets.get(brackets.size() - 1) == '{'))   return false;
-                brackets.remove(brackets.size()-1);
-            }
-            else if (c == ']') {
-                if (brackets.size() == 0)   return false;
-                else if (!(brackets.get(brackets.size() - 1) == '['))   return false;
-                brackets.remove(brackets.size()-1);
-            }
-        }
+        ArrayList<String> result = new ArrayList<String>();
 
-        return brackets.size() == 0;
+        String[] arr = str.split("");
+
+        HashSet<String> hashSet =  new HashSet<>(Arrays.asList(arr));
+        String[] newarr = hashSet.toArray(new String[0]);
+        Arrays.sort(newarr);
+
+        // 원소 갯수
+        // 0일 경우, 빈 리스트 추가
+        result.add(new String());
+        // 1 이상
+        for (int i = 1; i <= newarr.length; i++) {
+            //조합
+            combination(result, newarr, new boolean[newarr.length], 0, newarr.length, i);
+        }
+        Collections.sort(result);
+        return result;
     }
 
+    public static void combination(ArrayList<String> result, String[] arr, boolean visited[], int depth, int n, int r){
+        if (r == 0) {
+            ArrayList<String> s = new ArrayList<>();
+            for (int i = 0; i < visited.length; i++) {
+                if (visited[i]) {
+                    s.add(arr[i]);
+                }
+            }
+            result.add(String.join("", s));
+            return;
+        }
+
+        if(depth==n){
+            return;
+        }
+
+        visited[depth] = true;
+        combination(result, arr, visited,depth+1, n, r-1);
+
+        visited[depth] = false;
+        combination(result, arr, visited,depth+1, n, r);
+
+        return;
+    }
 
     public static void main(String[] args) {
-        boolean output = balancedBrackets("[](){}");
-        System.out.println(output); // --> true
+        ArrayList<String> output1 = powerSet("abc");
+        System.out.println(output1); // ["", "a", "ab", "abc", "ac", "b", "bc", "c"]
 
-        boolean output2 = balancedBrackets("[({})]");
-        System.out.println(output2); // --> true
-
-        boolean output3 = balancedBrackets("[(]{)}");
-        System.out.println(output3); // --> false
+        ArrayList<String> output2 = powerSet("jjump");
+        System.out.println(output2); // ["", "j", "jm", "jmp", "jmpu", "jmu", "jp", "jpu", "ju", "m", "mp", "mpu", "mu", "p", "pu", "u"]
   }
 }
