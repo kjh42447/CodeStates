8efdb9419ed51368be215b284bada8dd48480f16
kjh42447 <kjh42447@naver.com>
2022-08-29 18:00:06 +0900

    순열 순서 알고리즘

--- section1/Java/JavaTest/src/Test.java
+++ section1/Java/JavaTest/src/Test.java
@@ -1,60 +1,46 @@
+import java.lang.reflect.Array;
 import java.util.*;
 import java.util.stream.Collectors;
 
 public class Test {
 
-    public static ArrayList<String> powerSet(String str) {
+    public static int orderOfPresentation(int N, int[] K) {
         // TODO:
-        ArrayList<String> result = new ArrayList<String>();
+        int result = 0;
+        boolean[] visited = new boolean[N];
 
-        String[] arr = str.split("");
+        for (int i = 0; i < N; i++) {
+            int facCount = N-i-1;
+            int fac = 1;
+            int mulCount = K[i]-1;
 
-        HashSet<String> hashSet =  new HashSet<>(Arrays.asList(arr));
-        String[] newarr = hashSet.toArray(new String[0]);
-        Arrays.sort(newarr);
+            for (int j = 1; j <= facCount; j++) {
+                fac *= j;
+            }
 
-        // 원소 갯수
-        // 0일 경우, 빈 리스트 추가
-        result.add(new String());
-        // 1 이상
-        for (int i = 1; i <= newarr.length; i++) {
-            //조합
-            combination(result, newarr, new boolean[newarr.length], 0, newarr.length, i);
+            for (int j = 0; j < K[i]-1; j++) {
+                if (visited[j]){
+                    mulCount--;
+//                    System.out.println(j);
+                }
+            }
+            visited[K[i]-1] = true;
+            result += fac*mulCount;
+//            System.out.println(Arrays.toString(visited));
+//            System.out.printf("i : %d, K[i] : %d, mC = %d, fac : %d, re : %d\n", i, K[i], mulCount, fac, result);
         }
-        Collections.sort(result);
+
         return result;
     }
 
-    public static void combination(ArrayList<String> result, String[] arr, boolean visited[], int depth, int n, int r){
-        if (r == 0) {
-            ArrayList<String> s = new ArrayList<>();
-            for (int i = 0; i < visited.length; i++) {
-                if (visited[i]) {
-                    s.add(arr[i]);
-                }
-            }
-            result.add(String.join("", s));
-            return;
-        }
-
-        if(depth==n){
-            return;
-        }
-
-        visited[depth] = true;
-        combination(result, arr, visited,depth+1, n, r-1);
-
-        visited[depth] = false;
-        combination(result, arr, visited,depth+1, n, r);
-
-        return;
-    }
-
     public static void main(String[] args) {
-        ArrayList<String> output1 = powerSet("abc");
-        System.out.println(output1); // ["", "a", "ab", "abc", "ac", "b", "bc", "c"]
+        int output = orderOfPresentation(3, new int[]{2, 3, 1});
+        System.out.println(output); // 3
 
-        ArrayList<String> output2 = powerSet("jjump");
-        System.out.println(output2); // ["", "j", "jm", "jmp", "jmpu", "jmu", "jp", "jpu", "ju", "m", "mp", "mpu", "mu", "p", "pu", "u"]
+        output = orderOfPresentation(5, new int[]{1, 3, 2, 4, 5});
+        System.out.println(output); // 6
+
+        output = orderOfPresentation(7, new int[]{7, 6, 5, 4, 3, 2, 1});
+        System.out.println(output); // 6
   }
 }
